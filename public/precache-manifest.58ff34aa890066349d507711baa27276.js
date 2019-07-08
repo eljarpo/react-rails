c4ae442ad0ae03d20a9bd8370aa8827c70ff5709
Jorge Romussi <jromussi@gmail.com>
2019-07-08 14:55:13 -0400

    ready to first push heroku

---
+++ public/precache-manifest.58ff34aa890066349d507711baa27276.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c8a1cdcf083059c0a46063648b10121",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbdd884a359a1b8a545c",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1481ba6cd6399fee98b9",
+    "url": "/static/js/2.f4b65bbe.chunk.js"
+  },
+  {
+    "revision": "cbdd884a359a1b8a545c",
+    "url": "/static/js/main.df845271.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
